e8ab62a32f4c23674ef7b62ca022f21dd3bfceb6
Igor Steffano <igor.viana1@live.com>
2023-06-04 20:43:20 -0300

    Operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,10 @@
+import javax.sound.midi.Synthesizer;
+
+public class Operadores {
+    public static void main(String[] args) {
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+        System.out.println(nomeCompleto);
+        //resultado: LINGUAGEMJAVA
+        
+    }
+}
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,24 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //Estudar tipos Primitivos
+        //Estudar a classe String que representa texto na aplicação
+
+        double salarioMinimo = 2500;
+
+        short shortNumber = 1;
+        int normalNumber = shortNumber;
+        short shortNumber2 = (short) normalNumber;
+
+        int numero = 5;
+        numero = 20;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+
+        System.out.println(VALOR_DE_PI);
+
+        
+
     }
 }
